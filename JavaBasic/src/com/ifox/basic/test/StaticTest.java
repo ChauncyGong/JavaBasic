82db008fc3b1c0ccf774ada0649a2877e577371e
ChauncyGong <ChauncyGong@outlook.com>
2017-12-14 23:49:04 +0800

    12-14

---
+++ JavaBasic/src/com/ifox/basic/test/StaticTest.java
@@ -0,0 +1,40 @@
+package com.ifox.basic.test;
+
+public class StaticTest {
+	
+	
+	public static String type = "static";
+	
+	{
+		System.out.println("赋值 i = 10");
+		i = 10;
+	}
+	
+	public int i = 5;
+	
+	public StaticTest() {
+		System.out.println("构造前 i = " + i);
+		i = 15;
+		System.out.println("构造后 i = " + i);
+	}
+	
+	public static void print() {
+		System.out.println("static");
+	}
+	
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		
+        /**
+         * 编译时，字段赋值操作和初始化块都放在 构造器代码之前，至于谁先执行，取决于他们的相对位置
+         */
+		StaticTest s = new StaticTest(); 
+		
+		/**
+		 * 该类的null对象可以访问该类的静态成员,但不能直接使用 null.方法
+		 */
+		System.out.println(s.type);
+		s = null;
+		s.print();
+	}
+}
---
+++ JavaBasic/src/com/ifox/interview/Extend1.java
@@ -0,0 +1,37 @@
+package com.ifox.interview;
+
+public class Extend1 {
+	
+	public static void main(String[] args) {
+		System.out.println(new BB().getValue());
+	}
+	
+	static class AA {
+		protected int value;
+		public AA(int v) {
+			setValue(v);
+		}
+		public void setValue(int value) {
+			this.value = value;
+		}
+		public int getValue() {
+			try {
+				value ++;
+				return value;
+			} finally {
+				this.setValue(value);
+				System.out.println(value);
+			}
+		}
+	}
+	
+	static class BB extends AA {
+		public BB() {
+			super(5);
+			setValue(getValue() - 3);
+		}
+		public void setValue(int value) {        //方法覆盖
+			super.setValue(2*value);		//调用父类方法
+		}
+	}
+}
