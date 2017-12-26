3a74d927e9062bd4479a30d59c843d07e8dc6020
ChauncyGong <ChauncyGong@outlook.com>
2017-12-27 00:11:55 +0800

    12-27

--- JavaBasic/src/com/ifox/socket/SocketServer.java
+++ JavaBasic/src/com/ifox/socket/SocketServer.java
@@ -1,20 +1,20 @@
-package com.ifox.socket;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-
-public class SocketServer {
-	
-	public static void main(String[] args) {
-		try {
-			Socket socket = new Socket("127.0.0.1", 7878);
-			BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-}
+//package com.ifox.socket;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.net.Socket;
+//
+//public class SocketServer {
+//	
+//	public static void main(String[] args) {
+//		try {
+//			Socket socket = new Socket("127.0.0.1", 7878);
+//			BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//}
